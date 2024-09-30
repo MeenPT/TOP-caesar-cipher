5ff0040e10b21b19dbc3df8e8de426f2b3d5a405
Pakapol Tanachaisith <meen260600@gmail.com>
2024-09-30 15:51:05 +0700

    Lint the project

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
-def is_alphabet?(character)
-  return character.downcase.ord.between?('a'.ord, 'z'.ord)
+# frozen_string_literal: true
+
+def alphabet?(character)
+  character.downcase.ord.between?('a'.ord, 'z'.ord)
 end
 
 def encode_character(char, shift_factor = 1)
-  return char if !is_alphabet?(char)
+  return char unless alphabet?(char)
 
   ord = char.downcase.ord
   a_ord = 'a'.ord
   z_ord = 'z'.ord
 
-  if (ord + shift_factor).between?(a_ord, z_ord)
-    return (ord + shift_factor).chr
-  end
+  return (ord + shift_factor).chr if (ord + shift_factor).between?(a_ord, z_ord)
 
   new_ord = ((ord + shift_factor) % z_ord) + (a_ord - 1)
 
-  return new_ord.chr
+  new_ord.chr
 end
 
 def caesar_cipher(message = '', shift_factor = 1)
@@ -26,10 +26,10 @@ def caesar_cipher(message = '', shift_factor = 1)
 
     encoded_char = is_upcase ? encode_character(char, shift_factor).upcase : encode_character(char, shift_factor)
 
-    ecoded_message = ecoded_message + encoded_char
+    ecoded_message += encoded_char
   end
 
-  return ecoded_message
+  ecoded_message
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher('What a string!', 5)
